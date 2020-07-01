21ef244dfd33fe127cc084987674132c9f3a05ff
minimelonling <61599898+minimelonling@users.noreply.github.com>
2020-07-01 23:56:23 +0800

    Add files via upload

--- ml_play.py
+++ ml_play.py
@@ -1,6 +1,3 @@
-distance = 405
-targets = [315, 315, 315, 315]
-
 class MLPlay:
     def __init__(self, player):
         self.player = player
@@ -12,110 +9,120 @@ class MLPlay:
             self.player_no = 2
         elif self.player == "player4":
             self.player_no = 3
-        self.car_vel = 0
-        self.car_pos = (0, 0)
-        self.lanes = [35, 105, 175, 245, 315, 385, 455, 525, 595]
-        self.lane = 315
-        self.target = 315
+        self.car_vel = 0                            # speed initial
+        self.car_pos = (0,0)                        # pos initial
+        self.car_lane = self.car_pos[0] // 70       # lanes 0 ~ 8
+        self.lanes = [35, 105, 175, 245, 315, 385, 455, 525, 595]  # lanes center
         pass
 
     def update(self, scene_info):
-        for key in scene_info.keys():
-            print(key)
-        global flag
         """
-        Generate the command according to the received scene information
+        9 grid relative position
+        |    |    |    |
+        |  1 |  2 |  3 |
+        |    |  5 |    |
+        |  4 |  c |  6 |
+        |    |    |    |
+        |  7 |  8 |  9 |
+        |    |    |    |       
         """
-        self.car_pos = scene_info[self.player]
+        def check_grid():
+            grid = set()
+            speed_ahead = 100
+            if self.car_pos[0] <= 65: # left bound
+                grid.add(1)
+                grid.add(4)
+                grid.add(7)
+            elif self.car_pos[0] >= 565: # right bound
+                grid.add(3)
+                grid.add(6)
+                grid.add(9)
+
+            for car in scene_info["cars_info"]:
+                if car["id"] != self.player_no:
+                    x = self.car_pos[0] - car["pos"][0] # x relative position
+                    y = self.car_pos[1] - car["pos"][1] # y relative position
+                    if x <= 40 and x >= -40 :      
+                        if y > 0 and y < 300:
+                            grid.add(2)
+                            if y < 200:
+                                speed_ahead = car["velocity"]
+                                grid.add(5) 
+                        elif y < 0 and y > -200:
+                            grid.add(8)
+                    if x > -100 and x < -40 :
+                        if y > 80 and y < 250:
+                            grid.add(3)
+                        elif y < -80 and y > -200:
+                            grid.add(9)
+                        elif y < 80 and y > -80:
+                            grid.add(6)
+                    if x < 100 and x > 40:
+                        if y > 80 and y < 250:
+                            grid.add(1)
+                        elif y < -80 and y > -200:
+                            grid.add(7)
+                        elif y < 80 and y > -80:
+                            grid.add(4)
+            return move(grid= grid, speed_ahead = speed_ahead)
+            
+        def move(grid, speed_ahead): 
+            # if self.player_no == 0:
+            #     print(grid)
+            if len(grid) == 0:
+                return ["SPEED"]
+            else:
+                if (2 not in grid): # Check forward 
+                    # Back to lane center
+                    if self.car_pos[0] > self.lanes[self.car_lane]:
+                        return ["SPEED", "MOVE_LEFT"]
+                    elif self.car_pos[0 ] < self.lanes[self.car_lane]:
+                        return ["SPEED", "MOVE_RIGHT"]
+                    else :return ["SPEED"]
+                else:
+                    if (5 in grid): # NEED to BRAKE
+                        if (4 not in grid) and (7 not in grid): # turn left 
+                            if self.car_vel < speed_ahead:
+                                return ["SPEED", "MOVE_LEFT"]
+                            else:
+                                return ["BRAKE", "MOVE_LEFT"]
+                        elif (6 not in grid) and (9 not in grid): # turn right
+                            if self.car_vel < speed_ahead:
+                                return ["SPEED", "MOVE_RIGHT"]
+                            else:
+                                return ["BRAKE", "MOVE_RIGHT"]
+                        else : 
+                            if self.car_vel < speed_ahead:  # BRAKE
+                                return ["SPEED"]
+                            else:
+                                return ["BRAKE"]
+                    if (self.car_pos[0] < 60 ):
+                        return ["SPEED", "MOVE_RIGHT"]
+                    if (1 not in grid) and (4 not in grid) and (7 not in grid): # turn left 
+                        return ["SPEED", "MOVE_LEFT"]
+                    if (3 not in grid) and (6 not in grid) and (9 not in grid): # turn right
+                        return ["SPEED", "MOVE_RIGHT"]
+                    if (1 not in grid) and (4 not in grid): # turn left 
+                        return ["SPEED", "MOVE_LEFT"]
+                    if (3 not in grid) and (6 not in grid): # turn right
+                        return ["SPEED", "MOVE_RIGHT"]
+                    if (4 not in grid) and (7 not in grid): # turn left 
+                        return ["MOVE_LEFT"]    
+                    if (6 not in grid) and (9 not in grid): # turn right
+                        return ["MOVE_RIGHT"]
+                                
+                    
+        if len(scene_info[self.player]) != 0:
+            self.car_pos = scene_info[self.player]
+
         for car in scene_info["cars_info"]:
             if car["id"]==self.player_no:
                 self.car_vel = car["velocity"]
+
         if scene_info["status"] != "ALIVE":
             return "RESET"
-        if self.encounter_threat(scene_info):
-            self.change_lane(scene_info)
-        return self.move(scene_info)
-        return ["MOVE_LEFT", "MOVE_RIGHT", "SPEED", "BRAKE"]
-
-    def print_scene(self, scene_info):
-        for i in scene_info.keys():
-            if i == "computer_cars":
-                print("computer_cars:")
-                for j in scene_info[i]:
-                    print("           " + str(j))
-            elif i == "cars_info":
-                print("cars_info: ")
-                for j in scene_info[i]:
-                    print(j)
-            else:
-                print(str(i) + ": " + str(scene_info[i]))
-
-    def encounter_threat(self, scene_info):
-        global distance
-        global flag
-        group = []
-        closest = None
-        for car in scene_info["cars_info"]:
-            if len(self.car_pos) != 0 and car["pos"][0] == self.lane and car["id"] != self.player_no and car["pos"][1] < self.car_pos[1]:
-                group.append(car)
-        for car in group:
-            if closest is None or closest["pos"][1] < car["pos"][1]:
-                closest = car
-        if closest != None and self.car_pos[1] - closest["pos"][1] < distance:
-            return  True
-        return False
-
-    def change_lane(self, scene_info):
-        print("change")
-        group = []
-        closest = None
-        min_time = 0
-        for car in scene_info["cars_info"]:
-            if car["pos"][1] < self.car_pos[1]:
-                if car["pos"][0] == self.car_pos[0] - 70:
-                    group.append(car)
-                elif car["pos"][0] == self.car_pos[0] +70:
-                    group.append(car)
-        for car in group:
-            if closest == None or (self.car_pos[1] - car["pos"][1]) / (self.car_vel - car["velocity"]) < (self.car_pos[1] - closest["pos"][1]) / (self.car_vel - closest["velocity"]):
-                closest = car
-        if closest != None:
-            if closest["pos"][0] == self.car_pos[0] - 70:
-                if self.lane == 595:
-                    self.target = self.lane - 70
-                else:
-                    self.target = self.lane + 70
-            else:
-                if self.lane == 35:
-                    self.target = self.lane + 70
-                else:
-                    self.target = self.lane - 70
-        else:
-            if self.lane <= 315:
-                self.target = self.lane + 70
-            else:
-                self.target = self.lane - 70
-        self.lane = self.target
-        global targets
-        targets[self.player_no] = self.target
-            
-    def move(self, scene_info):
-        global targets
-        for car in scene_info["cars_info"]:
-            if len(self.car_pos) != 0 and car["pos"][0] == self.target and self.car_pos[1] - car["pos"][1] < 85 and self.car_pos[1] - car["pos"][1] > -85:
-                return ["SPEED"]
-            elif len(self.car_pos) != 0 and car["pos"][0] - self.car_pos[0] < 45 and car["pos"][0] - self.car_pos[0] > 40 and car["pos"][1] - self.car_pos[1] < 85 and car["pos"][1] - self.car_pos[1] > -85:
-                self.lane = self.lane - 70
-                return ["SPEED"]
-            elif len(self.car_pos) != 0 and car["pos"][0] - self.car_pos[0] > -45 and car["pos"][0] - self.car_pos[0] < -40 and car["pos"][1] - self.car_pos[1] < 85 and car["pos"][1] - self.car_pos[1] > -85:
-                self.lane = self.lane + 70
-                return ["SPEED"]
-        if scene_info["frame"] > 5 and len(self.car_pos) != 0 and self.lane < self.car_pos[0]:
-            return ["MOVE_LEFT", "SPEED"]
-        elif scene_info["frame"] > 5 and len(self.car_pos) != 0 and self.lane > self.car_pos[0]:
-            return ["MOVE_RIGHT", "SPEED"]
-        else:
-            return ["SPEED"]
+        self.car_lane = self.car_pos[0] // 70
+        return check_grid()
 
     def reset(self):
         """
